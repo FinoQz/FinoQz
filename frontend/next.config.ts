528512246ff091f0090af506e1ab977e14cfe08c
FinoQz <info.finoqz@gmail.com>
2025-10-27 03:42:48 +0530

    final commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   // Optional: basePath if needed
   // basePath: '/landing',
 }
